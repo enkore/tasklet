f7d935ad0b07e7d11a93ef72449ed500cdf49260
enkore <public@enkore.de>
2014-04-25 17:06:07 +0200

    You can even use this, right now. That was simpler than I expected.

--- static/script.js
+++ static/script.js
@@ -1,6 +1,26 @@
 $(function() {
-    $("section").dblclick(function() {
-        alert(this);
+    function enter_edit_mode() {
+        var section = $(this);
+        var tasklet = section.text().trim();
+        var input_field = $("<input type='text'>").attr("value", tasklet);
 
+        section.empty();
+        section.off("dblclick");
+        section.append(input_field);
+
+        input_field.keyup(function(event) {
+            if(event.keyCode == 13) { // enter
+                window.location.pathname = "/change/" + encodeURIComponent(tasklet) + "/" + encodeURIComponent(input_field.val());
+            }
+        });
+
+    }
+
+    $("section").dblclick(enter_edit_mode);
+
+    $(".new input").keyup(function(event) {
+        if(event.keyCode == 13) { // enter
+            window.location.pathname = "/add/" + encodeURIComponent($(this).val());
+        }
     });
 });
