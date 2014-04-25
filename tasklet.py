bef5227fb52bb6fc3a35451155815f7145ab9805
enkore <public@enkore.de>
2014-04-25 16:47:18 +0200

    Let's just use a simple, plain-text file for this.
    One line per todo. Correctly en/decodes stuff like \n
    That's the way it'd be.

--- tasklet.py
+++ tasklet.py
@@ -1,4 +1,5 @@
-import shelve
+import re
+import collections
 from enum import Enum
 
 from flask import Flask, g, redirect, url_for, render_template
@@ -7,21 +8,49 @@ DB_FILE = "tasklet"
 
 
 class Priorities(Enum):
-    Green = 0
-    Blue = 1
-    Yellow = 2
-    Red = 3
+    green = 0
+    blue = 1
+    yellow = 2
+    red = 3
 
 
 class Tasklet:
-    text = ""
-    priority = Priorities.Green
+    _text = ""
+    priority = Priorities.green
     done = False
 
-    def __init__(self, text, priority=Priorities.Green, done=False):
+    def __init__(self, text):
         self.text = text
-        self.priority = priority
-        self.done = done
+
+    @property
+    def text(self):
+        return self._text
+
+    @text.setter
+    def text(self, text):
+        self._text = text
+        markers = re.findall(":([a-zA-Z]+)", text)
+
+        for marker in markers:
+            if marker in Priorities.__members__:
+                self.priority = Priorities[marker]
+            if marker in ["done", "closed", "fixed", "resolved"]:
+                self.done = True
+
+
+class TaskletDB(collections.UserList):
+    def __init__(self):
+        super().__init__()
+
+        with open(DB_FILE, "rb") as db_file:
+            for line in db_file:
+                self.append(Tasklet(line.decode("unicode_escape").strip()))
+
+    def close(self):
+        with open(DB_FILE, "wb") as db_file:
+            for tasklet in self:
+                db_file.write(tasklet.text.encode("unicode_escape"))
+                db_file.write(b"\n")
 
 app = Flask(__name__)
 
@@ -36,30 +65,27 @@ def close_db(exception):
 def get_db():
     db = getattr(g, "_db", None)
     if db is None:
-        db = g._db = shelve.open(DB_FILE, writeback=True)
-        if "tasklets" not in db:
-            db["tasklets"] = []
+        g._db = db = TaskletDB()
     return db
 
 
 @app.route("/")
 def list_tasklets():
     return render_template("list_tasklets.html",
-                           tasklets=get_db()["tasklets"])
+                           tasklets=get_db())
 
 
-@app.route("/add/<int:priority>/<text>")
-def add_tasklet(priority, text):
-    get_db()["tasklets"].append(Tasklet(text, Priorities(priority)))
+@app.route("/add/<text>")
+def add_tasklet(text):
+    get_db().append(Tasklet(text))
     return redirect(url_for("list_tasklets"))
 
 
-@app.route("/done/<text>")
-def mark_done(text):
-    for tasklet in get_db()["tasklets"]:
+@app.route("/change/<text>/<new>")
+def mark_done(text, new):
+    for tasklet in get_db():
         if tasklet.text == text:
-            tasklet.done = True
-            get_db().sync()
+            tasklet.text = new
             return redirect(url_for("list_tasklets"))
 
 
